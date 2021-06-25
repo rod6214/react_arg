47730fcbc8e26f673b785d77dd05628bd74b0d34
Nelson Amador <rod6214@gmail.com>
2021-06-25 17:21:36 -0300

    release 1.4.4

--- src/tests/main.test.js
+++ src/tests/main.test.js
@@ -1,14 +1,8 @@
-import React from "react-dom";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-import main from "../../main"
+import main from "../../main";
 
 describe('Main test', () => {
 
     test("Can run a message", () => {
-        // render(<App/>);
         expect(main.run()).toBe('In progress');
-
-        // console.log(index)
     });
 });
