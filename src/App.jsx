ccb0af06323bf86fed5b924173b4da4384cc20df
Nelson Amador <rod6214@gmail.com>
2021-06-28 09:59:26 -0300

    release 1.6.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import './App.scss';
+
 const App = () => {
     return (<div>ARG Framework</div>)
 }
--- src/tests/config.test.js
+++ src/tests/config.test.js
@@ -3,14 +3,14 @@ import pkg from "../../package.json";
 describe('Main test', () => {
 
     test("Build command is configured in the package", () => {
-        expect(pkg.bin.xbuild).toBe('bin/exec_build.js');
+        expect(pkg.bin.xbuild, "Build command is not in the folder").toBe('bin/efxec_build.js');
     });
 
     test("Server command is configured in the package", () => {
-        expect(pkg.bin.xserver).toBe('bin/exec_server.js');
+        expect(pkg.bin.xserver, "Server command is not in the folder").toBe('bin/exec_server.js');
     });
 
     test("Test command is configured in the package", () => {
-        expect(pkg.bin.xtest).toBe('bin/exec_test.js');
+        expect(pkg.bin.xtest, "Test command is not in the folder").toBe('bin/exec_test.js');
     });
 });
